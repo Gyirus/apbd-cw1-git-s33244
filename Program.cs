26f842b04fc43857885ba5f742f0b18ff019da97
Gleb Karelin <s33244@pjwstk.edu.pl>
2026-03-18 23:43:09 +0100

    Add a header

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+        Console.WriteLine("=== APBD Git Exer ===");
+        
         Console.WriteLine("Podaj liczby oddzielone spacją:");
         string input = Console.ReadLine();
         
