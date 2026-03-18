184bc1746eb3a887bc6db7ea9ffc82b99315d94b
Gleb Karelin <s33244@pjwstk.edu.pl>
2026-03-18 23:02:51 +0100

    Add StatisticsHelper class with sum and average methods

---
+++ StatisticsHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp;
+
+public static class StatisticsHelper
+{
+    public static int CalculateSum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (var num in numbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+    
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers.Length == 0) return 0;
+        
+        int sum = CalculateSum(numbers);
+        return (double)sum / numbers.Length;
+    }
+}
